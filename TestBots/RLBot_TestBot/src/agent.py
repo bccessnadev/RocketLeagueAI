695fd6b5b00bebe8d50204f42ce32677d7dc1d71
bccessnadev <bccessna.dev@gmail.com>
2024-12-31 12:53:54 -0600

    Switched to rlgym_sim
    Implemented example implementation of rlgym_sim, ran the training briefly, and confirmed the rlbot version runs.
    
    rlgym_sim allows for exponentially faster training. Instead of running 1 version of the entire Rocket League game, it runs a barebones simulation of rocket league among many processes on the GPU. This allows for training 50x faster.
    It also uses wandb to log and graph  verbose details in realtime.
    
    Started work on an actual bot I am naming Ducky. Multiple stages of training have already been successfully performed.

--- TestBots/RLBot_TestBot/src/agent.py
+++ TestBots/RLBot_TestBot/src/agent.py
@@ -5,7 +5,7 @@ class Agent:
     def __init__(self):
         # Navigate back two folders and into the "models" folder
         cur_dir = os.path.dirname(os.path.realpath(__file__))
-        model_path = os.path.join(cur_dir, '..', '..', 'models', '1v1Bot.zip', '1v1Bot')
+        model_path = os.path.join(cur_dir, '..', '..', 'models', '1v1Bot.zip', '1v1Bot_50000000_steps')
         
         # Normalize the path to handle different operating systems
         model_path = os.path.normpath(model_path)
